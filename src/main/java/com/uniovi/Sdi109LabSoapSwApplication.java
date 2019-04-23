59207cd36e338cd814646c2b13c90ff6a033004b
UO244730 <UO244730@uniovi.es>
2019-04-23 17:03:57 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ src/main/java/com/uniovi/Sdi109LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi109LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi109LabSoapSwApplication.class, args);
+	}
+
+}
